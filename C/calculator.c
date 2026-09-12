384ba8186d0278eda6ff00bacbd34e305cbae286
Akshay <akshayveer615@gmail.com>
2026-09-12 09:49:39 +0530

    Remove space in sum output formatting

--- C/calculator.c
+++ C/calculator.c
@@ -6,7 +6,7 @@ int main() {
     printf("Enter two numbers: ");
     scanf("%d %d",&a,&b);
 
-    printf("Sum = %d\n", a + b);
+    printf("Sum = %d\n", a+b);
     printf("Difference = %d\n", a - b);
     printf("Product = %d\n", a * b);
 
