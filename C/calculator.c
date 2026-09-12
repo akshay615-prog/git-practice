65218728ae6682c63ce8d4ac871e5571a4b66bd1
Akshay <akshayveer615@gmail.com>
2026-09-12 09:47:50 +0530

    Fix formatting in scanf for input

--- C/calculator.c
+++ C/calculator.c
@@ -4,7 +4,7 @@ int main() {
     int a,b;
 
     printf("Enter two numbers: ");
-    scanf("%d %d",&a, &b);
+    scanf("%d %d",&a,&b);
 
     printf("Sum = %d\n", a + b);
     printf("Difference = %d\n", a - b);
