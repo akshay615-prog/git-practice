0039c63c26abed8724cc69f5b4332289986d4186
Akshay <akshayveer615@gmail.com>
2026-09-13 16:40:55 +0530

    Fix formatting of difference calculation output

--- C/calculator.c
+++ C/calculator.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d %d",&a,&b);
 
     printf("Sum = %d\n",a+b);
-    printf("Difference = %d\n", a - b);
+    printf("Difference = %d\n", a -b);
     printf("Product = %d\n", a * b);
 
     return 0;
